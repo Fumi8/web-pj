7c6a20b9558dd165ad58d2a55891e3c059717ed4
Fumi_N <fumikanakagawa8@gmail.com>
2021-10-05 11:52:30 +0900

    first commit

---
+++ experiment.js
@@ -0,0 +1,33 @@
+// const hikeExp = document.querySelector(".hike-exp");
+
+// window.addEventListener("scroll", scrollReveal);
+
+// function scrollReveal(){
+//     const hikePos = hikeExp.getBoundingClientRect().top;
+//     const windowHeight = window.innerHeight / 1.8;
+
+//     if (hikePos < windowHeight) {
+//         hikeExp.style.color = "red";
+//     }
+// }
+
+// // IntersectionObserver
+
+// const slide = document.querySelector(".hike");
+
+// let options = {
+//     threshold: 1
+// };
+
+// let observer = new IntersectionObserver(slideAnim, options);
+
+// function slideAnim(entrise){
+//     entrise.forEach(entry => {
+//         if(entry.isIntersecting){
+//             slide.style.background = "white";
+//         }
+//     });
+// }
+
+// observer.observe(slide);
+
